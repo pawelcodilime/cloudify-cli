262563b550924e4fb070f1251800102074ae3320
Avia Efrat <avia.efrat@gigaspaces.com>
2015-12-30 15:53:29 +0200

    Add `deployments create` logic to `cfy install`

--- cloudify_cli/commands/install.py
+++ cloudify_cli/commands/install.py
@@ -27,7 +27,7 @@ from cloudify_cli.commands import deployments
 def install(blueprint_path, blueprint_id, archive_location, blueprint_filename,
             deployment_id, inputs):
 
-    # if the `blueprint-id` wasn't supplied, the blueprint id will be the name
+    # if `blueprint-id` wasn't supplied, the blueprint id will be the name
     # of the directory that contains the main blueprint of the the app.
     if blueprint_id is None:
         blueprint_id = os.path.basename(
@@ -42,6 +42,11 @@ def install(blueprint_path, blueprint_id, archive_location, blueprint_filename,
         blueprints.publish_archive(archive_location, blueprint_filename,
                                    blueprint_id)
         pass
+
+    # if `deployment-id` wasn't supplied, the deployment id will be the same
+    # as the blueprint id.
+    if deployment_id is None:
+        deployment_id = blueprint_id
 
     deployments.create(blueprint_id, deployment_id, inputs)
 
