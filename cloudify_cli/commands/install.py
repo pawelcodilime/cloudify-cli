7046d72e943b9d3cd1446994f65ebd2b3e05b9f8
Avia Efrat <avia.efrat@gigaspaces.com>
2015-12-31 14:03:00 +0200

    Add `parameters` argument to `cfy install`'s handler

--- cloudify_cli/commands/install.py
+++ cloudify_cli/commands/install.py
@@ -25,7 +25,7 @@ from cloudify_cli.commands import deployments
 
 
 def install(blueprint_path, blueprint_id, archive_location, blueprint_filename,
-            deployment_id, inputs, workflow):
+            deployment_id, inputs, workflow, parameters):
 
     # if `blueprint-id` wasn't supplied, the blueprint id will be the name
     # of the directory that contains the main blueprint of the the app.
