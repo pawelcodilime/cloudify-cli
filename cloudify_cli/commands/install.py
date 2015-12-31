9fa6cd9c0281385e8c35bb58b7d8cb849c593c60
Avia Efrat <avia.efrat@gigaspaces.com>
2015-12-31 14:08:55 +0200

    Remove redundant TODO

--- cloudify_cli/commands/install.py
+++ cloudify_cli/commands/install.py
@@ -13,7 +13,6 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-# TODO make all the command docstrings consistent
 """
 Handles 'cfy install'
 """
